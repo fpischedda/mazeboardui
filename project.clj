1aeb24275559c4cf1f0d6ef81339bd9a1c111170
e_fpischedda <francesco.pischedda@abinsula.com>
2017-01-01 16:07:25 +0100

    initial work to integrate mazeboardui into mazeboard repo

--- project.clj
+++ project.clj
@@ -12,13 +12,13 @@
   :plugins [[lein-cljsbuild "1.1.1"]
             [lein-figwheel "0.5.0-3"]]
 
-  :source-paths ["src"]
+  :source-paths ["src/cljs"]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
   :cljsbuild {:builds
               [{:id "dev"
-                :source-paths ["src"]
+                :source-paths ["src/cljs"]
 
                 :figwheel {:on-jsload "mazeboardui.core/on-js-reload"}
 
@@ -31,7 +31,7 @@
                ;; production. You can build this with:
                ;; lein cljsbuild once min
                {:id "min"
-                :source-paths ["src"]
+                :source-paths ["src/cljs"]
                 :compiler {:output-to "resources/public/js/compiled/mazeboardui.js"
                            :main mazeboardui.core
                            :optimizations :advanced
